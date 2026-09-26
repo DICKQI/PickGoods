3d018198e6b591b920ee8e4d01c99907b60a29db
dickqi <dickqi87@gmail.com>
2026-09-26 10:13:26 +0800

    fix: debug

--- backend/ShiGu/settings.py
+++ backend/ShiGu/settings.py
@@ -38,7 +38,7 @@ if _dotenv_path.exists():
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
